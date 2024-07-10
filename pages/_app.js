1d07b99a1e86560d268967da101e476f8533d7b4
Robinson <robyvuillemin@gmail.com>
2024-07-10 12:01:45 +0200

    head

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Next.js App</title>
+        <title>My Moviz</title>
       </Head>
       <Component {...pageProps} />
     </>
