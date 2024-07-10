280e06c8f7fae9756ae81bd6328def4763f1e939
Robinson <robyvuillemin@gmail.com>
2024-07-10 11:51:27 +0200

    first

---
+++ components/Home.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react';
+import { Popover, Button } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import Movie from './Movie';
+import 'antd/dist/antd.css';
+import styles from '../styles/Home.module.css';
+
+function Home() {
+  const [likedMovies, setLikedMovies] = useState([]);
+  const [TMDBRes, setTMDBRes] = useState([])
+
+  // Liked movies (inverse data flow)
+  const updateLikedMovies = (movieTitle) => {
+    if (likedMovies.find(movie => movie === movieTitle)) {
+      setLikedMovies(likedMovies.filter(movie => movie !== movieTitle));
+    } else {
+      setLikedMovies([...likedMovies, movieTitle]);
+    }
+  };
+
+  const likedMoviesPopover = likedMovies.map((data, i) => {
+    return (
+      <div key={i} className={styles.likedMoviesContainer}>
+        <span className="likedMovie">{data}</span>
+        <FontAwesomeIcon icon={faCircleXmark} onClick={() => updateLikedMovies(data)} className={styles.crossIcon} />
+      </div>
+    );
+  });
+
+  const popoverContent = (
+    <div className={styles.popoverContent}>
+      {likedMoviesPopover}
+    </div>
+  );
+
+  // Movies list
+  useEffect(() => {
+    fetch('http://localhost:3000/movies')
+    .then (response=> response.json())
+    .then (data => setTMDBRes(data.results))
+  }, [])
+  
+  const reduceText = (text, maxLength) => {
+    if(text.length <= maxLength){
+      return text;
+    }
+    return text.substring(0,maxLength)+'...';
+  }
+
+  const movies = TMDBRes.map((data, i) => {
+    const isLiked = likedMovies.some(movie => movie === data.title);
+    return <Movie key={i} updateLikedMovies={updateLikedMovies} isLiked={isLiked} title={data.title} overview={reduceText(data.overview,250)} poster_path={data.poster_path} voteAverage={data.voteAverage} voteCount={data.voteCount} />;
+  });
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <div className={styles.logocontainer}>
+          <img src="logo.png" alt="Logo" />
+          <img className={styles.logo} src="logoletter.png" alt="Letter logo" />
+        </div>
+        <Popover title="Liked movies" content={popoverContent} className={styles.popover} trigger="click">
+          <Button>♥ {likedMovies.length} movie(s)</Button>
+        </Popover>
+      </div>
+      <div className={styles.title}>LAST RELEASES</div>
+      <div className={styles.moviesContainer}>
+        {movies}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ components/Movie.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faStar, faVideo } from '@fortawesome/free-solid-svg-icons';
+import styles from '../styles/Movie.module.css';
+
+function Movie(props) {
+  const [watchCount, setWatchCount] = useState(0);
+  const [personalNote, setPersonalNote] = useState(0);
+
+  // Average evaluation
+  const stars = [];
+  for (let i = 0; i < 10; i++) {
+    let style = {};
+    if (i < props.voteAverage - 1) {
+      style = { 'color': '#f1c40f' };
+    }
+    stars.push(<FontAwesomeIcon key={i} icon={faStar} style={style} />);
+  }
+
+  // Watch movie
+  const handleWatchMovie = () => {
+    setWatchCount(watchCount + 1);
+  };
+  let videoIconStyle = { 'cursor': 'pointer' };
+  if (watchCount > 0) {
+    videoIconStyle = { 'color': '#e74c3c', 'cursor': 'pointer' };
+  }
+
+  // Like movie
+  const handleLikeMovie = () => {
+    props.updateLikedMovies(props.title);
+  };
+  let heartIconStyle = { 'cursor': 'pointer' };
+  if (props.isLiked) {
+    heartIconStyle = { 'color': '#e74c3c', 'cursor': 'pointer' };
+  }
+
+  // Personal note
+  const personalStars = [];
+  for (let i = 0; i < 10; i++) {
+    let style = { 'cursor': 'pointer' };
+    if (i < personalNote) {
+      style = { 'color': '#2196f3', 'cursor': 'pointer' };
+    }
+    personalStars.push(<FontAwesomeIcon key={i} icon={faStar} onClick={() => setPersonalNote(i + 1)} style={style} className="note" />);
+  }
+
+  return (
+    <div className={styles.card}>
+      <img className={styles.image} src={`https://image.tmdb.org/t/p/w500/${props.poster_path}`} alt={props.title} />
+      <div className={styles.textContainer}>
+        <div>
+          <span className={styles.name}>{props.title}</span>
+          <p className={styles.description}>{props.overview}</p>
+        </div>
+        <div className={styles.iconContainer}>
+          <span className={styles.vote}>{stars} ({props.voteCount})</span>
+          <span>{personalStars} ({personalNote})</span>
+          <span><FontAwesomeIcon icon={faVideo} onClick={() => handleWatchMovie()} style={videoIconStyle} className="watch" /> ({watchCount})</span>
+          <span><FontAwesomeIcon icon={faHeart} onClick={() => handleLikeMovie()} style={heartIconStyle} className="like" /></span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Movie;
